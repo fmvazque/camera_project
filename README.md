cdd407807444c6a39f06df6866527963d8d660ca
Fabio Vazquez <fabiovazquez@outlook.com>
2016-10-15 17:23:46 -0700

    code cleanup and documentation update on how to start the processes

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # camera_project
 camera 
+
+In order to start up the camera monitoring system, we need to start two different processes:
+
+1. Open a SSH session on Raspberry Pi 
+2. start the camera_main.py process:
+     nohup sudo python camera_mail.py &
+3. start node.js front-end site:
+     nohup sudo npm start &
+
+You can safely close the SSH remote session as the processes will continue running on your Pi.
--- camera_main.py
+++ camera_main.py
@@ -6,70 +6,79 @@ from datetime import datetime
 
 import logging
 import os
+import time
 
-logging.basicConfig(level=logging.DEBUG, filename="/tmp/camera_main.log")
+def main():
+    print ("starting... ")
+    
+    logging.basicConfig(level=logging.DEBUG, filename="/tmp/camera_main.log")
 
-maxMean = 0
-#cam = Camera(prop_set={'width': 1024, 'height': 768})
-cam = Camera()
+    #cam = Camera(prop_set={'width': 1024, 'height': 768})
+    maxMean = 0
+    cam = Camera()
 
-#display = Display((1024, 768))
+    #display = Display((1024, 768))
 
-try:
     while True:
-        #display.checkEvents()
-
-        # grabs original image to be used when motion is detected
-        original = cam.getImage()
-        timestamp = datetime.now().strftime("%a, %b, %-d %Y %H:%M:%S")
-        original.drawText(timestamp, 10, 10, color=Color.ORANGE, fontsize=20)
+        tstart = time.time()
         
-        # takes two shots in sequence (we will compare them to detect motion)        
-        img01 = cam.getImage().toGray()
-        time.sleep(0.5)
-        img02 = cam.getImage().toGray()
+        doCapture(cam, maxMean)
+        
+        tfinish = time.time()
+        print ("time ellapsed: {0}".format(tfinish - tstart))
 
-        # 
-        diff = (img01 - img02).binarize(50).invert()
-        diff.show()
 
-        matrix = diff.getNumpy()
+def doCapture(cam, maxMean):
+    # grabs original image to be used when motion is detected
+    original = cam.getImage()
+    timestamp = datetime.now().strftime("%a, %b, %-d %Y %H:%M:%S")
+    original.drawText(timestamp, 10, 10, color=Color.ORANGE, fontsize=20)
+    
+    # takes two shots in sequence (we will compare them to detect motion)        
+    img01 = cam.getImage().toGray()
+    time.sleep(0.5)
+    img02 = cam.getImage().toGray()
 
-        mean = matrix.mean()
-        print (mean)
+    # 
+    diff = (img01 - img02).binarize(50).invert()
+    #diff.show()
 
-        if (mean > maxMean):
-           maxMean = mean
+    matrix = diff.getNumpy()
 
-        # saves current picture, which can be used to be shown on the UI
-        original.save('./pics/current/current.png')    
+    mean = matrix.mean()
+    print (mean)
 
-        # if image diff is above threshold, save a copy of the image for later analysis
-        if mean >= 0.1:
-            date_path = datetime.now().strftime("%Y/%m/%d")
-            dir_name = "./pics/{0}".format(date_path)
+    if (mean > maxMean):
+        maxMean = mean
 
-            # create directory if if does not exist
-            if not os.path.exists(dir_name):
-                os.makedirs(dir_name)            
+    # saves current picture, which can be used to be shown on the UI
+    original.save('./pics/current/current.png')    
 
-            epoch = int((datetime.now() - datetime(1970,1,1)).total_seconds())
-            filename = "image_{0}".format(epoch)
-            
-            # original.save('./pics/' + filename + ".png")
-            file_full_name = "{0}/{1}.png".format(dir_name, filename)
+    # if image diff is above threshold, save a copy of the image for later analysis
+    if mean >= 0.1:
+        date_path = datetime.now().strftime("%Y/%m/%d")
+        dir_name = "./pics/{0}".format(date_path)
 
-            print("saving file [{0}]".format(file_full_name)) 
-            original.save(file_full_name)
+        # create directory if if does not exist
+        if not os.path.exists(dir_name):
+            os.makedirs(dir_name)            
 
-            # saves binarized images for analysis
-            # diff.save('./pics/binarized/' + filename + "_diff_mean_{0}.png".format(mean))
-            
-            print ('images saved')
-            print ('max mean so far: {0}'.format(maxMean))
+        epoch = int((datetime.now() - datetime(1970,1,1)).total_seconds())
+        filename = "image_{0}".format(epoch)
+        
+        # original.save('./pics/' + filename + ".png")
+        file_full_name = "{0}/{1}.png".format(dir_name, filename)
 
-except:
-    logging.exception("exception raised!!!")
+        print("saving file [{0}]".format(file_full_name)) 
+        original.save(file_full_name)
+
+        # saves binarized images for analysis
+        # diff.save('./pics/binarized/' + filename + "_diff_mean_{0}.png".format(mean))
+        
+        print ('images saved')
+        print ('max mean so far: {0}'.format(maxMean))
+
+main()
 
 ##img1 = cam.getImage().invert()
 ##img1.save('image_invert.png')
